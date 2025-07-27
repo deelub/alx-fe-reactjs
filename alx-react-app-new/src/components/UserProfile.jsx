c3853b7cd43773effb9975d2dd752721ca0d6eab
Divine Lubiku <Divinelubiku6@gmail.com>
2025-07-27 20:36:48 +0200

    Changes to padding made

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserProfile = (props) => {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
       <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
       <p style={{ fontSize: '18px' }}>
         Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
