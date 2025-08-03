362677cfea753fe8ec48e54b435ed66c4dce4555
Divine Lubiku <Divinelubiku6@gmail.com>
2025-08-03 21:44:34 +0200

    Initial commit of dir

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+// src/App.jsx
 import React from 'react';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     </div>
   );
 }
+
+
 
 /*function App() {
   const [count, setCount] = useState(0)
