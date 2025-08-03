2048883f6320491c664944fee7fa302a745a6a2b
Divine Lubiku <Divinelubiku6@gmail.com>
2025-08-03 23:49:11 +0200

    Added terms

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,17 +3,23 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',                  // <-- Add searchTerm state
+  setSearchTerm: (term) => set({ searchTerm: term }), // <-- Action to update searchTerm
+
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));
